dadd14f974f735b4b387b321909a7564914dae0b
Janek <jankozinski@wp.pl>
2024-02-20 11:53:27 +0100

    Add real data to seed

--- FootballLeague/Data/Seed.cs
+++ FootballLeague/Data/Seed.cs
@@ -28,19 +28,40 @@ namespace FootballLeague.Data
                                     {
                                         new Match()
                                         {
-                                            Date = "00.00.0000",
-                                            Time = "00:00",
-                                            Team1 = "team1",
-                                            Team2 = "team2",
-                                            Score = "2:1"
+                                            Date = "23-07-2022",
+                                            Time = "16:00",
+                                            Team1 = "Charleroi",
+                                            Team2 = "Eupen",
+                                            Score = "3:1"
                                         },
                                         new Match()
                                         {
-                                            Date = "00.00.0000",
-                                            Time = "00:00",
-                                            Team1 = "team1",
-                                            Team2 = "team2",
-                                            Score = "2:1"
+                                            Date = "23-07-2022",
+                                            Time = "18:15",
+                                            Team1 = "Kortrijk",
+                                            Team2 = "OH Leuven",
+                                            Score = "0:2"
+                                        }
+                                    }
+                                },
+                                new Queue() {
+                                    Matches = new List<Match>()
+                                    {
+                                        new Match()
+                                        {
+                                            Date = "23-07-2022",
+                                            Time = "18:15",
+                                            Team1 = "Zulte",
+                                            Team2 = "RfC Seresien",
+                                            Score = "2:0"
+                                        },
+                                        new Match()
+                                        {
+                                            Date = "23-07-2022",
+                                            Time = "20:45",
+                                            Team1 = "Saint-Truiden",
+                                            Team2 = "Union Saint-Gilloise",
+                                            Score = "1:1"
                                         }
                                     }
                                 }
